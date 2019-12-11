3560de4be67f1762189bc53fc9f0c0f23639600f
lyzengar <darthmaul85@hotmail.com>
2019-12-11 15:00:53 -0800

    build nav component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="nav-bar">
+        <h3>Taiko</h3>
+        <h3>Stands</h3>
+        <h3>Misc.</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+
+  }
+};
+</script>
