a29b4dbae62dcc4693d4f0c419907e34588908a6
lyzengar <darthmaul85@hotmail.com>
2019-12-12 15:12:12 -0800

    build nav component

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,11 @@
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-
+import NavBar from "./components/NavBar.vue";
 export default {
   name: "app",
   components: {
+    NavBar,
     // HelloWorld
   }
 };
