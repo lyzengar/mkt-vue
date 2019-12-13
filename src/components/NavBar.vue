ae362adbd8ec997840bd223fc02047e5d4461634
lyzengar <darthmaul85@hotmail.com>
2019-12-13 15:01:29 -0800

    nav css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="nav-bar">
-        <h3>Taiko</h3>
-        <h3>Stands</h3>
-        <h3>Misc.</h3>
+        <div class="nav-head">
+            <h2>Makoto Taiko Equipment</h2>
+            <h2>真鼓音</h2>
+        </div>
+        <div class="nav-sections">
+            <h3>Taiko</h3>
+            <h3>Stands</h3>
+            <h3>Misc.</h3>
+        </div>
     </div>
 </template>
 
