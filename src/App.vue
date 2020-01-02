e184197710f3bde52c45381630e1350537f7e991
lyzengar <darthmaul85@hotmail.com>
2020-01-02 15:03:10 -0800

    incorporate pic component

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 // import HelloWorld from "./components/HelloWorld.vue";
 import NavBar from "./components/NavBar.vue";
 import Main from "./components/Main.vue";
+import Pic from "./components/Pic.vue";
 export default {
   name: "app",
   components: {
