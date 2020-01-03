090a2ac06d270a542120d6e584ea98b67f2d951e
lyzengar <darthmaul85@hotmail.com>
2020-01-03 14:58:54 -0800

    clean unused code

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
 // import HelloWorld from "./components/HelloWorld.vue";
 import NavBar from "./components/NavBar.vue";
 import Main from "./components/Main.vue";
-import Pic from "./components/Pic.vue";
 export default {
   name: "app",
   components: {
