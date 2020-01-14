a137dce9206a24ee049836c537c29744b694ee4a
lyzengar <darthmaul85@hotmail.com>
2020-01-14 14:59:44 -0800

    add taiko assets

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@ let taiko = [
           {menu: "Crestone 2", caption: "C2", URL: "https://imgur.com/R0z9RgX.jpg"},
           {menu: "Crestone 3", caption: "C3", URL: "https://imgur.com/RFkynuP.jpg"},
           {menu: "Big Dana", caption: "", URL: "https://imgur.com/BEDGpGu.jpg"},
+          {menu: "Masala", caption: "", URL: "https://imgur.com/HqGtMdG.jpg"},
 
         ];
 </script>
